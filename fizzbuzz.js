2f7ac2f08434a33ca8fbd4d1657c2af4d17d6312
Akash Thakur <skyatwork007@gmail.com>
2016-12-01 17:03:17 +0530

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(){
+for(i=0;i<20;i++){
+if(i%3==0)
+console.log("fizz")
+
+else if(i%5==0)
+console.log("buzz")
+else if((i%3==0)&&(i%5==0))
+console.log("fizzbizz")
+else console.log(i)
+}
+}
+
+fizzbuzz()
+
